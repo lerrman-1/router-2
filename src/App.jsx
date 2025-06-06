7c01df96e2a39eaa075499397ae9081d1c8ae10a
Luis Elias Rodriguez <lerrman52@gmail.com>
2025-06-06 18:38:19 -0400

    se modificó el main

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ function App() {
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/products' element={<Products />} />
-
         </Routes>
 
 
